252684bcfbab601c16e49c0d95db4725aeb1b7a9
PETKOV Aleksandar <aleksandar.petkov@soprasteria.com>
2025-10-16 13:44:43 +0300

    PR-00-Add-product-promotion-user-services

---
+++ users/src/test/java/com/promo/radar/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.promo.radar.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
