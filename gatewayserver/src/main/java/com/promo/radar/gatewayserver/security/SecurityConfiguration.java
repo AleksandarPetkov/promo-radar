ee89cbd336b0690be7b238eef5120714d320b7e2
PETKOV Aleksandar <aleksandar.petkov@soprasteria.com>
2025-11-05 15:52:06 +0200

    Implement role-based authorization using Keycloak roles

--- gatewayserver/src/main/java/com/promo/radar/gatewayserver/security/SecurityConfiguration.java
+++ gatewayserver/src/main/java/com/promo/radar/gatewayserver/security/SecurityConfiguration.java
@@ -2,11 +2,16 @@ package com.promo.radar.gatewayserver.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -18,12 +23,28 @@ public class SecurityConfiguration {
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchanges -> exchanges
                         .pathMatchers(HttpMethod.GET).permitAll()
-                        .pathMatchers("/api/datacollector/**").authenticated()
+                        .pathMatchers("/api/datacollector/**").hasRole("KAFKA_SEND_MESSAGE")
                         .anyExchange().permitAll()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(Customizer.withDefaults())
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(
+                                grantedAuthoritiesExtractor())
+                        )
                 )
                 .build();
+    }
+
+    /**
+     * Creates a converter that extracts granted authorities (roles)
+     * from a Keycloak JWT token and adapts them to Spring Security’s
+     * reactive authentication model.
+     *
+     * @return a converter that maps JWT claims to authentication authorities
+     */
+    private Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
+        JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
+        jwtConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRoleConverter());
+
+        return new ReactiveJwtAuthenticationConverterAdapter(jwtConverter);
     }
 }
